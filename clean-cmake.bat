e678cc9dad16cc1aa15cf2b32192ddad2dee8101
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-27 16:23:05 +0100

    Start script in minimized format and add clean-cmake.bat file

---
+++ clean-cmake.bat
@@ -0,0 +1,8 @@
+del ALL_BUILD.vcxproj
+del ALL_BUILD.vcxproj.filters
+del cmake_install.cmake
+del CMakeCache.txt
+del Project.sln
+del ZERO_CHECK.vcxproj
+del ZERO_CHECK.vcxproj.filters
+rd /s /q CMakeFiles
