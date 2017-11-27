d4d2e175407469768c1a0c6f5255914d1dcb1a24
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-27 16:09:24 +0100

    Add commentary

--- build.bat
+++ build.bat
@@ -1,9 +1,11 @@
+:: Move to sources folder
 cd src/main/go
 
-
+:: Compile all Go sources
 go tool compile main.go
 
-
+:: Link the executable
 go tool link -o ../../../pbrain-gogomoku.exe main.o
 
+:: Return to origin
 cd ../../../
