15c6915198cce8004aaaa0029cdc82df76ef9d61
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-30 17:03:27 +0100

    Refractor code

--- src/gogomoku/ai/start.go
+++ src/gogomoku/ai/start.go
@@ -40,96 +40,93 @@ func posIsAvailable(pos Position) bool {
 	return false
 }
 
+func calcWeightByStarPart(pos Position, player int, weightToSet *int) bool {
+	if posIsValid(pos) {
+		if GameBoard[pos.X][pos.Y] == player {
+			*weightToSet++
+			return true
+		} else if GameBoard[pos.X][pos.Y] == otherPlayer(player) {
+			return false
+		}
+		return true
+	}
+	return false
+}
+
 func calcWeightByStar(position Position, turn int) int {
 	var weight = [8]int{0, 0, 0, 0, 0, 0, 0, 0}
 	func(weightToSet *int) {
-		for i := 0; i < 4; i++ {
+		for i := 1; i < 5; i++ {
 			newPos := Position{position.X - i, position.Y}
-			if posIsValid(newPos) && GameBoard[newPos.X][newPos.Y] == turn {
-				*weightToSet++
-			} else {
+			if calcWeightByStarPart(newPos, turn, weightToSet) == false {
 				break
 			}
 		}
 	}(&weight[0])
 	func(weightToSet *int) {
-		for i := 0; i < 4; i++ {
+		for i := 1; i < 5; i++ {
 			newPos := Position{position.X - i, position.Y + i}
-			if posIsValid(newPos) && GameBoard[newPos.X][newPos.Y] == turn {
-				*weightToSet++
-			} else {
+			if calcWeightByStarPart(newPos, turn, weightToSet) == false {
 				break
 			}
 		}
 	}(&weight[1])
 	func(weightToSet *int) {
-		for i := 0; i < 4; i++ {
+		for i := 1; i < 5; i++ {
 			newPos := Position{position.X, position.Y + i}
-			if posIsValid(newPos) && GameBoard[newPos.X][newPos.Y] == turn {
-				*weightToSet++
-			} else {
+			if calcWeightByStarPart(newPos, turn, weightToSet) == false {
 				break
 			}
 		}
 	}(&weight[2])
 	func(weightToSet *int) {
-		for i := 0; i < 4; i++ {
+		for i := 1; i < 5; i++ {
 			newPos := Position{position.X + i, position.Y + i}
-			if posIsValid(newPos) && GameBoard[newPos.X][newPos.Y] == turn {
-				*weightToSet++
-			} else {
+			if calcWeightByStarPart(newPos, turn, weightToSet) == false {
 				break
 			}
 		}
 	}(&weight[3])
 	func(weightToSet *int) {
-		for i := 0; i < 4; i++ {
+		for i := 1; i < 5; i++ {
 			newPos := Position{position.X + i, position.Y}
-			if posIsValid(newPos) && GameBoard[newPos.X][newPos.Y] == turn {
-				*weightToSet++
-			} else {
+			if calcWeightByStarPart(newPos, turn, weightToSet) == false {
 				break
 			}
 		}
 	}(&weight[4])
 	func(weightToSet *int) {
-		for i := 0; i < 4; i++ {
+		for i := 1; i < 5; i++ {
 			newPos := Position{position.X + i, position.Y - i}
-			if posIsValid(newPos) && GameBoard[newPos.X][newPos.Y] == turn {
-				*weightToSet++
-			} else {
+			if calcWeightByStarPart(newPos, turn, weightToSet) == false {
 				break
 			}
 		}
 	}(&weight[5])
 	func(weightToSet *int) {
-		for i := 0; i < 4; i++ {
+		for i := 1; i < 5; i++ {
 			newPos := Position{position.X, position.Y - i}
-			if posIsValid(newPos) && GameBoard[newPos.X][newPos.Y] == turn {
-				*weightToSet++
-			} else {
+			if calcWeightByStarPart(newPos, turn, weightToSet) == false {
 				break
 			}
 		}
 	}(&weight[6])
 	func(weightToSet *int) {
-		for i := 0; i < 4; i++ {
+		for i := 1; i < 5; i++ {
 			newPos := Position{position.X - i, position.Y - i}
-			if posIsValid(newPos) && GameBoard[newPos.X][newPos.Y] == turn {
-				*weightToSet++
-			} else {
+			if calcWeightByStarPart(newPos, turn, weightToSet) == false {
 				break
 			}
 		}
 	}(&weight[7])
-	return weight[0] ^ 3 + weight[1] ^ 3 + weight[2] ^ 3 + weight[3] ^ 3 +
-		weight[4] ^ 3 + weight[5] ^ 3 + weight[6] ^ 3 + weight[7] ^ 3
+	return weight[0] ^ 4 + weight[1] ^ 4 + weight[2] ^ 4 + weight[3] ^ 4 +
+		weight[4] ^ 4 + weight[5] ^ 4 + weight[6] ^ 4 + weight[7] ^ 4
 }
 
 // SI ON CHANGE MAX PROFONDEUR, IL FAUT LA CHANGER ICI
 func calcBestPosition(pos Position, profondeur int) int {
 	if GameBoard[pos.X][pos.Y] == 0 {
-		return (calcWeightByStar(pos, 1) + calcWeightByStar(pos, 2)) * profondeur + calcAllBestPosition(pos, profondeur-1)
+		return calcWeightByStar(pos, 1) ^ profondeur + calcWeightByStar(pos, 2) ^ profondeur + calcAllBestPosition(pos, profondeur-1)
 	}
 	return 0
 }
@@ -176,17 +173,17 @@ func calcAllBestPosition(origin Position, profondeur int) int {
 		weight[7] = calcBestPosition(pos[7], profondeur)
 	}
 
-	var max = weight[0]
+	var max = 0
 	for i := 1; i < 8; i++ {
-		if weight[i] > max {
-			max = weight[i]
+		if weight[i] > weight[max] {
+			max = i
 		}
 	}
-	return max
+	return weight[max]
 }
 
 func bestPosition(origin Position, profondeur int) Position {
-	var weight = [8]int{0, 0, 0, 0, 0, 0, 0, 0}
+	var weight = [8]int{-1, -1, -1, -1, -1, -1, -1, -1}
 	var pos = [8]Position{
 		{origin.X - 1, origin.Y},
 		{origin.X - 1, origin.Y + 1},
@@ -235,8 +232,8 @@ func firstTurn() Position {
 }
 
 func turn() Position {
-	var enemyWeight int = -1
-	var playerWeight int = -1
+	var enemyWeight = -1
+	var playerWeight = -1
 	if hasPlayed(LastPlayerPosition) {
 		playerWeight = calcWeightByStar(LastPlayerPosition, 1)
 	}
